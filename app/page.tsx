be86cf436c9febd00846f25f63d07acea227ec42
mohofagh <56514540+mohofagh@users.noreply.github.com>
2026-08-07 00:30:25 +0330

    after gallery

--- app/page.tsx
+++ app/page.tsx
@@ -82,6 +82,12 @@ export default function Home() {
           >
             p5-sandbox
           </Link>
+          <Link 
+            href="/gallery" 
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+          >
+            Gallery
+          </Link>
         </div>
         
       </main>
